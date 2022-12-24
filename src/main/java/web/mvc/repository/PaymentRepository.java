e26a8f378420f0ea26f92d55316ad2c923b045cf
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-12-24 23:33:18 +0900

    221224 PaymentRepository 생성

---
+++ src/main/java/web/mvc/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package web.mvc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import web.mvc.domain.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long>{
+
+}
