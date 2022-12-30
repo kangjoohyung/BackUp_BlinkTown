7e0f407184c96f1cdf151b0e78c23625f557f23a
kangjoohyung89 <kangjoohyung89@gmail.com>
2022-12-30 10:59:25 +0900

    221230 payment 조회 쿼리문 생성

--- src/main/java/web/mvc/repository/PaymentRepository.java
+++ src/main/java/web/mvc/repository/PaymentRepository.java
@@ -1,9 +1,17 @@
 package web.mvc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import web.mvc.domain.Orders;
 import web.mvc.domain.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long>{
 
+	/**
+	 * Orders(컬럼은 orders_no) orders 로 payment List받기
+	 * 최근 순으로 조회
+	 */
+	List<Payment> findByOrdersOrderByPaymentDateDesc(Orders orders);
 }
